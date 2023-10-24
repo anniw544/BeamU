7dbccbdc0d270a668f2c6e3eeddc9b6fc04b93a0
William Annis <anniw544@hsd.k12.or.us>
2023-10-24 13:07:15 -0700

    new progecttime

--- Assets/scrips/PlayerMovement.cs
+++ Assets/scrips/PlayerMovement.cs
@@ -7,12 +7,20 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
-    public Transform orientation;
+    public float groundDrag
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+
+    public transform orientation;
 
     float horizontalInput;
     float verticalInput;
 
-    Vector3 moveDirection
+    Vector3 moveDirection;
 
     Rigidbody rb;
 
@@ -22,5 +30,38 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
     
     }
-    // https://youtu.be/f473C43s8nE?feature=shared&t=228
+   
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+   private void Update()
+   {
+    // groundcheck
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f +0.2f, whatIsGround);
+
+        MyInput();
+
+        //handel drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag=0;
+   }
+
+   private void MyInput()
+   {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+   }
+
+   private void MovePlayer()
+   {
+        //calulate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+   }
+   // https://youtu.be/f473C43s8nE?feature=shared&t=362
 }
